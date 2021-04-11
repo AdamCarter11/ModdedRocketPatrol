74e0370ad1b0504b7ac65b41eed29bfbdc356321
Adam <57573881+AdamCarter11@users.noreply.github.com>
2021-04-11 12:59:27 -0700

    fixedSomething

--- rocketPatrolRemake/src/Scenes/Play.js
+++ rocketPatrolRemake/src/Scenes/Play.js
@@ -87,7 +87,6 @@ class Play extends Phaser.Scene{
     shipExplode(ship){
         ship.alpha = 0;
         let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
-        this.sound.play('explosion');
         boom.anims.play('explode');
         boom.on('animationcomplete',()=>{
             ship.reset();
@@ -96,5 +95,6 @@ class Play extends Phaser.Scene{
         });
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+        this.sound.play('explosion');
     }
 }
