c3f960999dcf9f72996156d35d0350ac366f3ddc
Adam <57573881+AdamCarter11@users.noreply.github.com>
2021-04-18 12:06:51 -0700

    t

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -46,7 +46,7 @@ class Play extends Phaser.Scene{
 
 
         let timeConfig = {fontFamily: 'Courier', fontSize: '28px', backgroundColor: '#F3B141', color: '#843605', align: 'right', padding:{top: 5, bottom: 5,}, fixedWidth: 100}
-        this.timeLeft = this.add.text(borderUISize + boarderPadding + 100, borderUISize + boarderPadding*2, this.clock,timeConfig);
+        this.timeLeft = this.add.text(borderUISize + boarderPadding + 100, borderUISize + boarderPadding*3, this.clock,timeConfig);
     }
     update(){
         if(this.gameOver && (Phaser.Input.Keyboard.JustDown(keyR) || Phaser.Input.Keyboard.JustDown(keyF))){
